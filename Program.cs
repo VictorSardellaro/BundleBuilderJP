44fde4d87ba07c510dde74bcaa4fdeb4c55365de
VictorSardellaro <victor.henryk@outlook.com>
2021-08-05 14:50:11 -0300

    Backup_and_Zip

--- Program.cs
+++ Program.cs
@@ -15,13 +15,14 @@ namespace BundleBuilderJP
         private static void Load()
         {
             System.Console.Clear();
-            System.Console.WriteLine("Bundle Builder");
+            System.Console.WriteLine("Bundle Builder Tools");
             System.Console.WriteLine("------------------");
             System.Console.WriteLine("Chose an option");
             System.Console.WriteLine();
             System.Console.WriteLine("1 - Stop NP6");
             System.Console.WriteLine("2 - Clear NP6");
             System.Console.WriteLine("3 - Backup Bundle");
+            System.Console.WriteLine("4 - Zip Bundle");
             System.Console.WriteLine();
             System.Console.WriteLine();
             var option = short.Parse(Console.ReadLine()!);
@@ -33,6 +34,12 @@ namespace BundleBuilderJP
                     break;
                 case 2:
                     ActionsNP6.Stop();
+                    break;
+                case 3:
+                    ActionsNP6.Backup();
+                    break;
+                case 4:
+                    ActionsNP6.Zip();
                     break;
 
                 default: Load(); break;
--- Services/ActionsNP6.cs
+++ Services/ActionsNP6.cs
@@ -1,5 +1,9 @@
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+
+
 
 namespace BundleBuilderJP.Services.ActionsNP6
 {
@@ -38,10 +42,39 @@ namespace BundleBuilderJP.Services.ActionsNP6
             process.WaitForExit();
             System.Console.WriteLine(process.StandardOutput.ReadToEnd());
 
-
         }
 
+        public static void Backup()
+        {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+            try
+            {
+                File.Copy(sourcePath, targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
 
+        public static void Zip()
+        {
 
+            string startPath = @"D:\TEMP\A\Teste";
+            string zipPath = @"D:\TEMP\B";
+            string extractPath = @"D:\TEMP\C";
+
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+        }
     }
 }
+
