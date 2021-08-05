cd40d137228b217da54e18845e977e62e85692bd
VictorSardellaro <victor.henryk@outlook.com>
2021-08-05 13:24:44 -0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BundleBuilderJP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tring sourcePath = @"c:\temp\file1.txt";
+            //string targetPath = @"c:\temp\file2.txt";
+
+            // ProcessStartInfo ps = new ProcessStartInfo();
+            // ps.FileName = "cmd.exe";
+            // ps.WindowStyle = ProcessWindowStyle.Normal;
+            // ps.Arguments = @"/k C:\NewPOS61\2.Stop.cmd";
+            // Process.Start(ps);
+
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            process.StandardInput.WriteLine(@"C:\NewPOS61\2.Stop.cmd");
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+            process.Start();
+            process.StandardInput.WriteLine(@"C:\NewPOS61\3.ClearAll.bat");
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+            process.WaitForExit();
+            System.Console.WriteLine(process.StandardOutput.ReadToEnd());
+            Console.ReadKey();
+
+        }
+    }
+}
