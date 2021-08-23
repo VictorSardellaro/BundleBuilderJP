a401a0394b3efcb61ed8a7884b8c9757926084a6
VictorSardellaro <victor.henryk@outlook.com>
2021-08-23 13:48:38 -0300

    ConfigurationAdjusts

--- Serialization/Configuration.cs
+++ Serialization/Configuration.cs
@@ -7,37 +7,24 @@ namespace BundleBuilderJP.Serialization
 {
     public class Configuration
     {
-        public string startPathBackcup { get; set; }
+        public string StartPathBackup { get; set; }
         public string TargetZipPathBackup { get; set; }
         public string StartZipPathExtract { get; set; }
         public string TargetZipPathExtract { get; set; }
 
-        public static List<BundleBuilderJP.Serialization.Configuration> Serialization()
+        public static ConfigurationJson Serialization()
         {
             var json = File.ReadAllText(@"D:\Repository\BundleBuilderJP\Repositories\Config.json");
-            var configJson = JsonConvert.DeserializeObject<List<Configuration>>(json);
+            var configJson = JsonConvert.DeserializeObject<ConfigurationJson>(json);
+            
 
             return configJson;
 
         }
 
-        public static List<string> Paths()
+        public class ConfigurationJson
         {
-
-            var configJson = Serialization();
-
-            string startPathBackup = (configJson[0].startPathBackcup.ToString());
-            string StartZipPathExtract = (configJson[0].StartZipPathExtract.ToString());
-            string TargetZipPathBackup = (configJson[0].TargetZipPathBackup.ToString());
-            string TargetZipPathExtract = (configJson[0].TargetZipPathExtract.ToString());
-
-            List<string> ConfigPaths = new List<string>();
-            ConfigPaths.Add(startPathBackup);
-            ConfigPaths.Add(StartZipPathExtract);
-            ConfigPaths.Add(TargetZipPathBackup);
-            ConfigPaths.Add(TargetZipPathExtract);
-
-            return ConfigPaths;
+            public Configuration Configuration { get; set; }
 
         }
     }
